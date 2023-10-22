cd120b917d48a4b3fbd256134d901f654496207f
Daniel <d.brummerloh@gmail.com>
2023-10-22 23:34:47 +0200

    add ripple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
-
+import { RippleModule } from 'primeng/ripple';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonListComponent } from './button-list/button-list.component';
@@ -15,6 +15,7 @@ import { ButtonComponent } from './button/button.component';
     AppRoutingModule,
     ButtonModule,
     BrowserAnimationsModule,
+    RippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
