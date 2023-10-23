7caf60005dec4040142e827b70ed09da7d9895b5
Daniel <d.brummerloh@gmail.com>
2023-10-23 22:33:22 +0200

    set window.confetti() to onclick of confetti button

--- src/app/button-confetti/button-confetti.component.ts
+++ src/app/button-confetti/button-confetti.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { WindowService } from '../window.service';
 
 @Component({
   selector: 'app-button-confetti',
   templateUrl: './button-confetti.component.html',
-  styleUrls: ['./button-confetti.component.css']
+  styleUrls: ['./button-confetti.component.css'],
 })
 export class ButtonConfettiComponent {
+  window: any = null;
 
+  constructor(private windowService: WindowService) {
+    this.window = this.windowService.nativeWindow;
+  }
 }
