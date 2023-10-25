47ec5b6cac49b0a6c1ac427649f499a2c96fa779
Daniel <d.brummerloh@gmail.com>
2023-10-25 21:29:40 +0200

    remove everything unused in ButtonListComponent

--- src/components/button-list/button-list.component.ts
+++ src/components/button-list/button-list.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AppLogicService } from 'src/app/app-logic.service';
-import { DataInitService } from 'src/services/data-init.service';
-import { ButtonData, StringObject } from 'src/shared/shared-types';
 import { AppStoreService } from 'src/shared/store';
 
 @Component({
@@ -10,30 +7,9 @@ import { AppStoreService } from 'src/shared/store';
   templateUrl: './button-list.component.html',
   styleUrls: ['./button-list.component.css'],
 })
-export class ButtonListComponent implements OnInit {
-  buttons: ButtonData[] = [];
-  correctResultMapping: StringObject = {};
-  nTotal: number = 10;
-  nSolved: number = 0;
-  selectedIndex: number | null = null;
-
+export class ButtonListComponent {
   nSolved$ = this.store.nSolved$;
+  buttons$ = this.store.buttons$;
 
-  constructor(
-    private appLogic: AppLogicService,
-    private dataInit: DataInitService,
-    private store: AppStoreService
-  ) {}
-
-  ngOnInit() {
-    let state = this.store.state();
-    this.nSolved = state.nSolved;
-    this.nTotal = state.nTotal;
-    this.correctResultMapping = state.correctResultMapping;
-    this.buttons = state.buttons;
-  }
-
-  onClick(newIndex: number) {
-    this.appLogic.selectItem(newIndex);
-  }
+  constructor(private store: AppStoreService) {}
 }
