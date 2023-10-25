17a628aec382b04f1f8cbce7a85cf67f8266a02b
Daniel <d.brummerloh@gmail.com>
2023-10-25 20:19:29 +0200

    use app logic from service and store

--- src/components/button-list/button-list.component.ts
+++ src/components/button-list/button-list.component.ts
@@ -17,6 +17,8 @@ export class ButtonListComponent implements OnInit {
   nSolved: number = 0;
   selectedIndex: number | null = null;
 
+  nSolved$ = this.store.nSolved$;
+
   constructor(
     private appLogic: AppLogicService,
     private dataInit: DataInitService,
@@ -24,54 +26,14 @@ export class ButtonListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.appLogic.reload();
-
-    this.dataInit.refreshQuizData();
     let state = this.store.state();
-
     this.nSolved = state.nSolved;
     this.nTotal = state.nTotal;
     this.correctResultMapping = state.correctResultMapping;
     this.buttons = state.buttons;
   }
 
-  handleClick(newIndex: number) {
-    // reset buttons that are not done
-    for (let button of this.buttons) {
-      if (button.state != 'done') {
-        button.state = '';
-      }
-    }
-
-    // only one selected
-    if (this.selectedIndex == null) {
-      this.selectedIndex = newIndex;
-      this.buttons[newIndex].state = 'selected';
-      console.log(this.buttons);
-      return;
-    }
-
-    // same button clicked again
-    if (this.selectedIndex == newIndex) {
-      this.selectedIndex = null;
-      return;
-    }
-
-    let selectedName = this.buttons[this.selectedIndex].label;
-    let newName = this.buttons[newIndex].label;
-
-    if (this.correctResultMapping[selectedName] == newName) {
-      // correct pair selected
-      this.buttons[newIndex].state = 'done';
-      this.buttons[this.selectedIndex].state = 'done';
-      this.selectedIndex = null;
-      return;
-    } else {
-      // incorrect pair selected
-      this.buttons[newIndex].state = 'red';
-      this.buttons[this.selectedIndex].state = 'red';
-      this.selectedIndex = null;
-      return;
-    }
+  onClick(newIndex: number) {
+    this.appLogic.selectItem(newIndex);
   }
 }
