bd4610145ae6f89457f90331fd0ce2569935a508
Daniel <d.brummerloh@gmail.com>
2023-10-25 17:19:22 +0200

    remove approuter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { AppStoreService } from 'src/shared/store';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonConfettiComponent } from './button-confetti/button-confetti.component';
 import { ButtonListComponent } from './button-list/button-list.component';
@@ -20,7 +19,6 @@ import { WindowService } from './window.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ButtonModule,
     DividerModule,
     BrowserAnimationsModule,
