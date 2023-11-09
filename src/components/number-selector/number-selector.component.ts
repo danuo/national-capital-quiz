e9d82b4fac6b7899f768a1d8578b1da0458242c4
Daniel <d.brummerloh@gmail.com>
2023-11-09 15:56:13 +0100

    make number-selector one file

--- src/components/number-selector/number-selector.component.ts
+++ src/components/number-selector/number-selector.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 import { AppStoreService } from 'src/services/app-store.service';
 
+const componentTemplate = `
+<div class="flex w-full flex-wrap justify-content-center align-items-center">
+  <p-button
+    icon="pi pi-minus"
+    [outlined]="true"
+    (click)="decrement()"
+  ></p-button>
+  <div class="relative" style="width: 4rem">
+    <div
+      class="absolute h-full w-full flex justify-content-center align-items-center"
+    >
+      <i class="pi pi-sort-alt" style="font-size: 1.1rem"></i>
+    </div>
+  </div>
+  <p-button
+    icon="pi pi-plus"
+    [outlined]="true"
+    (click)="increment()"
+  ></p-button>
+</div>
+`;
+
 @Component({
   selector: 'app-number-selector',
-  templateUrl: './number-selector.component.html',
-  styleUrls: ['./number-selector.component.css'],
+  template: componentTemplate,
+  styles: [],
 })
 export class NumberSelectorComponent {
   nMax: number = 10;
